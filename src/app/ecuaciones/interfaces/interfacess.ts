a68377032518c8f210f821ca5aac78869f59266b
Luis Villarreal <ldvo9606@gmail.com>
2018-11-10 17:28:04 -0600

    updated interfaces

--- src/app/ecuaciones/interfaces/interfacess.ts
+++ src/app/ecuaciones/interfaces/interfacess.ts
@@ -1,27 +1,30 @@
 interface Element {
-    letter: string;
-    subscript: number;
+  letter: string;
+  subscript: number;
 }
 
 interface Compound {
-    constant: number;
-    elements: Element[];
+  constant: number|undefined;
+  elements: Element[];
 }
 
 export enum Difficulty {
-    EASY, MEDIUM, HARD
+  EASY,
+  MEDIUM,
+  HARD
 }
 
 interface Equation {
-    compounds: Compound[];
+  compounds: Compound[];
 }
 
 interface Problem {
-    equation1: Equation;
-    equation2: Equation;
-    difficulty: Difficulty;
+  equation1: Equation;
+  equation2: Equation;
+  difficulty: Difficulty;
+  solution: number[];
 }
 
 export interface Game {
-    problems: Problem[];
+  problems: Problem[];
 }
