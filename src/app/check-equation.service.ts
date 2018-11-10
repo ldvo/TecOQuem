79969ad9870a1808d9200d4c24ac836d99b0b8e9
Tony Toussaint <tonyyasi@gmail.com>
2018-11-10 17:25:58 -0600

    Create service to check if a user provided answer is correct

--- src/app/check-equation.service.ts
+++ src/app/check-equation.service.ts
@@ -5,5 +5,17 @@ import { Injectable } from '@angular/core';
 })
 export class CheckEquationService {
 
+  validateAnswer(actualAnswer: number[], userAnswer: number[]) : boolean {
+    if (actualAnswer.length !== userAnswer.length) {
+        return false;
+    }
+    for (let i = actualAnswer.length; i--;) {
+        if(actualAnswer[i] !== userAnswer[i]) {
+            return false;
+        }
+    }
+    return true;
+  }
+
   constructor() { }
 }
