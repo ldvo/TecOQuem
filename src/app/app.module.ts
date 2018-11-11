0cb724cd65c65bb76a2ca35b55be39f612eac83a
Tony Toussaint <tonyyasi@gmail.com>
2018-11-10 18:45:13 -0600

    Add needed things for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import {AppComponent} from './app.component';
 import {EcuacionesComponent} from './ecuaciones/ecuaciones.component';
 import {InicioComponent} from './inicio/inicio.component';
 import {ReaccionesComponent} from './reacciones/reacciones.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,9 @@ import {ReaccionesComponent} from './reacciones/reacciones.component';
   imports: [
     MatCardModule, BrowserModule, BrowserAnimationsModule, AppRoutingModule,
     MatToolbarModule, FlexLayoutModule, MatSidenavModule, MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Main Angular fire module 
+    AngularFireDatabaseModule  // Firebase database module 
   ],
   providers: [],
   bootstrap: [AppComponent]
