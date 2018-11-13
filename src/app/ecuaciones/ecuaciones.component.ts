9c65978cd9b925921e6ae413f8707d4823174d33
Luis Villarreal <ldvo9606@gmail.com>
2018-11-12 19:24:58 -0600

    added equation stuff

--- src/app/ecuaciones/ecuaciones.component.ts
+++ src/app/ecuaciones/ecuaciones.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
-import {Difficulty, Game} from './interfaces/interfacess';
+import {CheckEquationService} from '../check-equation.service';
+
+import {Difficulty, Game, Problem} from './interfaces/interfacess';
 
 @Component({
   selector: 'app-ecuaciones',
@@ -11,38 +13,143 @@ export class EcuacionesComponent implements OnInit {
   phase = 0;
   nombre = '';
   game: Game = {
-    problems: [{
-      difficulty: Difficulty.EASY,
-      equation1: {
-        compounds:
-            [
-              {
-                constant: undefined,
-                elements:
-                    [{letter: 'P', subscript: 4}, {letter: 'O', subscript: 10}]
-              },
-              {
-                constant: undefined,
-                elements:
-                    [{letter: 'H', subscript: 2}, {letter: 'O', subscript: 1}]
-              }
-            ]
-      },
-      equation2: {
-        compounds: [{
-          constant: 4,
-          elements:
+    problems: [
+      {
+        difficulty: Difficulty.EASY,
+        equation1: {
+          compounds:
               [
-                {letter: 'H', subscript: 3}, {letter: 'P', subscript: 1},
-                {letter: 'O', subscript: 4}
+                {
+                  constant: undefined,
+                  elements:
+                      [
+                        {letter: 'P', subscript: 4},
+                        {letter: 'O', subscript: 10}
+                      ]
+                },
+                {
+                  constant: undefined,
+                  elements:
+                      [{letter: 'H', subscript: 2}, {letter: 'O', subscript: 1}]
+                }
               ]
-        }]
+        },
+        equation2: {
+          compounds: [{
+            constant: 4,
+            elements:
+                [
+                  {letter: 'H', subscript: 3}, {letter: 'P', subscript: 1},
+                  {letter: 'O', subscript: 4}
+                ]
+          }]
+        },
+        solution: [2, 4]
       },
-      solution: [4]
-    }]
+      {
+        difficulty: Difficulty.EASY,
+        equation1: {
+          compounds:
+              [
+                {
+                  constant: undefined,
+                  elements:
+                      [
+                        {letter: 'H', subscript: 2},
+                        {letter: 'O', subscript: undefined}
+                      ]
+                },
+                {constant: 2, elements: [{letter: 'N', subscript: 2}]}
+              ]
+        },
+        equation2:
+            {
+              compounds:
+                  [{
+                    constant: 2,
+                    elements:
+                        [
+                          {letter: 'H', subscript: 3},
+                          {letter: 'P', subscript: 1},
+                          {letter: 'O', subscript: 4}
+                        ]
+                  }]
+            },
+        solution: [4]
+      }
+    ]
   };
 
-  constructor() {}
+  currentProblem: Problem|undefined;
+  currentProblemIndex = 0;
+  currentProblemAnswers: number[];
+  currentAnswerIndex = 0;
+  eq1AnswerIndexMap = {};
+  eq2AnswerIndexMap = {};
+  totalCorrectAnswers = 0;
 
-  ngOnInit() {}
+  constructor(private checkEquationService: CheckEquationService) {}
+
+  ngOnInit() {
+    this.setUpProblem(this.game.problems[this.currentProblemIndex]);
+  }
+
+  setUpProblem(p: Problem) {
+    this.currentProblem = undefined;
+    this.currentAnswerIndex = 0;
+    this.eq1AnswerIndexMap = {};
+    this.eq2AnswerIndexMap = {};
+    const eq1AnswerCount = p.equation1.compounds
+                               .filter((c) => {
+                                 if (c.constant === undefined) {
+                                   return true;
+                                 }
+                                 return false;
+                               })
+                               .length;
+    const eq2AnswerCount = p.equation2.compounds
+                               .filter((c) => {
+                                 if (c.constant === undefined) {
+                                   return true;
+                                 }
+                                 return false;
+                               })
+                               .length;
+    const totalAnswerCount = eq1AnswerCount + eq2AnswerCount;
+    this.currentProblemAnswers = new Array(totalAnswerCount);
+    this.currentProblem = p;
+  }
+
+  getCurrentAnswerIndex(e: number, i: number) {
+    if (e === 1) {
+      if (this.eq1AnswerIndexMap[i] === undefined) {
+        this.eq1AnswerIndexMap[i] = this.currentAnswerIndex++;
+      }
+      return this.eq1AnswerIndexMap[i];
+    }
+    if (this.eq2AnswerIndexMap[i] === undefined) {
+      this.eq2AnswerIndexMap[i] = this.currentAnswerIndex++;
+    }
+    return this.eq2AnswerIndexMap[i];
+  }
+
+  nextProblem() {
+    if (this.checkEquationService.validateAnswer(
+            this.currentProblem, this.currentProblemAnswers)) {
+      this.totalCorrectAnswers++;
+    }
+    this.currentProblemIndex++;
+    if (this.currentProblemIndex < this.game.problems.length) {
+      this.setUpProblem(this.game.problems[this.currentProblemIndex]);
+    } else {
+      this.phase = 3;
+    }
+  }
+
+  getScore() {
+    if (this.currentProblemIndex !== 0) {
+      return this.totalCorrectAnswers / this.currentProblemIndex * 100;
+    }
+    return 0;
+  }
 }
