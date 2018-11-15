9ff52cef8d0aa22e5b38c429427738f3488922fb
Luis Villarreal <ldvo9606@gmail.com>
2018-11-14 18:31:48 -0600

    me da weba esto 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import {NgModule} from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule} from '@angular/forms';
@@ -5,6 +6,10 @@ import {FormsModule} from '@angular/forms';
 import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+import {environment} from '../environments/environment';
 
 import {AcercaComponent} from './acerca/acerca.component';
 import {AppRoutingModule} from './app-routing.module';
@@ -13,10 +18,6 @@ import {EcuacionesComponent} from './ecuaciones/ecuaciones.component';
 import {InicioEcuacionesComponent} from './ecuaciones/inicio-ecuaciones/inicio-ecuaciones.component';
 import {InicioComponent} from './inicio/inicio.component';
 import {ReaccionesComponent} from './reacciones/reacciones.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { environment } from '../environments/environment';
   imports: [
     FormsModule, MatInputModule, MatFormFieldModule, MatCardModule,
     BrowserModule, BrowserAnimationsModule, AppRoutingModule, MatToolbarModule,
-    FlexLayoutModule, MatSidenavModule, MatButtonModule, MatIconModule, DragDropModule
+    FlexLayoutModule, MatSidenavModule, MatButtonModule, MatIconModule,
+    DragDropModule,
     AngularFireModule.initializeApp(environment.firebase, 'fcc-book-trading'),
     AngularFireDatabaseModule
   ],
