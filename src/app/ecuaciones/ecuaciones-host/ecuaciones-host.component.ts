d743226e7a9effd004fc4c56874f7d7daadd2d19
Luis Villarreal <ldvo9606@gmail.com>
2018-11-14 21:13:47 -0600

    me da weba esto 7

--- src/app/ecuaciones/ecuaciones-host/ecuaciones-host.component.ts
+++ src/app/ecuaciones/ecuaciones-host/ecuaciones-host.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {CreategameService} from '../../creategame.service';
-
+import {Player} from '../interfaces/interfacess';
+// tslint:disable
 @Component({
   selector: 'app-ecuaciones-host',
   templateUrl: './ecuaciones-host.component.html',
@@ -9,9 +10,20 @@ import {CreategameService} from '../../creategame.service';
 })
 export class EcuacionesHostComponent implements OnInit {
   idPartida: number;
+  players: Player[];
   constructor(
       private router: Router, private creategameService: CreategameService) {
     this.idPartida = Number(this.router.url.split('/').pop());
+    this.creategameService.fetchScores(this.idPartida).subscribe(d => {
+      const p: Player[] = [];
+      for (const key in d) {
+        let player: Player = {name: key, score: d[key]};
+        p.push(player);
+      }
+      this.players = p.sort((a, b) => {
+        return b.score - a.score;
+      });
+    });
   }
 
   ngOnInit() {}
