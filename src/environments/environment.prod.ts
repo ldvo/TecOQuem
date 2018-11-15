160f7e88571a4fc3323854455c1bd74a4e5997c9
Tony Toussaint <tonyyasi@gmail.com>
2018-11-15 08:44:17 -0600

    Add all firebase production stuffz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCGH6gfyjrGgb617WGSezo4QY21yWUH-Mc",
+    authDomain: "tecochem-c1556.firebaseapp.com",
+    databaseURL: "https://tecochem-c1556.firebaseio.com",
+    projectId: "tecochem-c1556",
+    storageBucket: "tecochem-c1556.appspot.com",
+    messagingSenderId: "198060561819"
+  }
 };
